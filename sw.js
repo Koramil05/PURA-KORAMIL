6bf4677a5c1c3cd7f676164f8cddc3d3bddb9043
sukasada05 <koramil05sukasada@gmail.com>
2026-03-27 19:17:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
 const CACHE_NAME = 'kas-app-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/offline.html',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://raw.githubusercontent.com/Koramil05/PURA05/main/favicon-32x32.png',
-    'https://raw.githubusercontent.com/Koramil05/PURA05/main/apple-touch-icon.png'
-];
+const urlsToCache = ['/', '/index.html', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'];
 
 self.addEventListener('install', event => {
     event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request).catch(() => {
-        return caches.match(event.request).then(response => {
-            if (response) return response;
-            if (event.request.mode === 'navigate') return caches.match('/offline.html');
-            return null;
-        });
-    }));
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
 
 self.addEventListener('activate', event => {
