95576c346d61c64789c90d1bf5d2bd94fe4080de
sukasada05 <koramil05sukasada@gmail.com>
2026-03-27 19:22:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,41 @@
 const CACHE_NAME = 'kas-app-v1';
-const urlsToCache = ['/', '/index.html', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'];
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
 
 self.addEventListener('install', event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then(response => {
+                if (response) return response;
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/offline.html');
+                }
+                return null;
+            });
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(caches.keys().then(cacheNames => {
-        return Promise.all(cacheNames.map(cache => { if (cache !== CACHE_NAME) return caches.delete(cache); }));
-    }));
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
